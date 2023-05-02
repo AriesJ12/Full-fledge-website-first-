89b19d9cde3516d71e3271bb765649298a9d04c7
kaiii07 <davidkian16@gmail.com>
2023-05-02 15:40:23 +0800

    Update login.php
    updated - login.php

--- restaurantWeb/Assets/login.php
+++ restaurantWeb/Assets/login.php
@@ -5,35 +5,37 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <link rel="stylesheet" href="css/loginStyle.css">
+    <link rel="stylesheet" href="css/loginstyle.css">
 </head>
 <body>
-    <div>
-        <?php
-        // error designn
-            if(isset($_GET['error']))
-            {
-                $error = $_GET['error'];
-                $user = $_GET['user'];
-                echo "<div><p>$error<p></div>";
-            }
-        ?>
-        <h1>Login page</h1>
-        <a href="../index.php">Home page</a>
-        <br>
-        <form name="loginForm" action="phpFunctions/login.inc.php" method="POST">
-            <label for="username">
-                Username
-            </label>
-            <input type="text" name="username" id="username" value = "<?php if(isset($_GET['user'])){echo "$user";}?>">
-            
-            <label for="password">
-                Password:
-            </label>
-            <input type="password" name="password" id="password">
+        <div>
+          <!--  <?php
+            // error designn
+                if(isset($_GET['error']))
+                {
+                    $error = $_GET['error'];
+                    $user = $_GET['user'];
+                    echo "<div><p>$error<p></div>";
+                }
+            ?> -->
+
+            <h1>Log In</h1>
+            <div class="about-logo">
+                <a href="../index.php" class="logo-content flex">
+                    <img src="images/logo-white.png" alt="logo">
+                </a> 
+            </div>
             <br>
-            <input type="submit" value = "Log in">    
-        </form>
-    </div>
+            <form name="loginForm" action="phpFunctions/login.inc.php" method="POST">
+                
+                <input type="text" name="username" placeholder="Enter your username" id="username" value = "<?php if(isset($_GET['user'])){echo "$user";}?>">
+                
+                <br>
+                <input type="password" placeholder="Enter your password" name="password" id="password">
+                <br>
+                <input type="submit" value = "Log in">    
+            </form>
+        </div>
+    <!-- <script src="js/script.js"></script>  -->
 </body>
 </html>
